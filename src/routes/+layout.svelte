f91bfcc961c8fdf82eb4a816ce80ea5b9504ac77
David Chu <sct.chu@gmail.com>
2024-04-24 21:50:33 +0200

    fix: footer always has a bottom padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,16 +7,8 @@
 		<slot />
 	</main>
 
-	<footer class="flex flex-col justify-center items-center">
+	<footer class="flex flex-col justify-center items-center pb-3">
 		<p class="text-xs">Powered by SDXL Lightning</p>
 		<p class="text-xs">Made from Spain with ❤️</p>
 	</footer>
 </div>
-
-<style>
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
