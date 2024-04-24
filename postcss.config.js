dff27a9c51c025c2e3dbcc724f757e296fff05f4
David Chu <sct.chu@gmail.com>
2024-04-24 21:46:57 +0200

    fix: optimize tailwind in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		...(process.env.VERCEL_ENV === 'production' ? { cssnano: {} } : {})
+	}
+};
